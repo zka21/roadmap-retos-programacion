a1c4a935aedbe45fa3a6f043712269be8c4bfc2b
ZhaidaCazasola <zhaidacazasola.20@gmail.com>
2024-10-21 19:25:26 -0500

    00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/zka21.py
@@ -0,0 +1,44 @@
+# Este es un comentario en python
+# https://docs.python.org/
+
+# Hay dos maneras de representar comentarios en python
+
+# 1. Sirve para hacer comentarios de una linea #
+# 2. Sirve para hacer comentario de varias lineas """ 
+
+"""Las tres comillas 
+sirven para hacer comentarios
+en python""" 
+
+'''También se puede hacer 
+comentarios de varias lineas 
+con las comillas simple'''
+#------------------------------------------------------------------------
+
+# Variable y Constante en python
+
+# 1. Variable: Python no requiere el tipo de dato antes del nombre de la variable
+# Ejemplos: 
+nombre = "Zhai"
+edad = 24
+altura = 1.60
+
+# 2. Constante: Pyhton no tiene un concepto acerca de constante como en otros lenguajes
+#              pero por convección las constantes se escriben en MAYUSCULA, indicando asi 
+#              que su valor no debe cambiar.
+# Ejemplos: 
+DNI = 3222344
+DIRECCION = "Jr. Alfonso Ugarte"
+#------------------------------------------------------------------------
+
+#Tipos de datos en Python
+
+numero = 1
+flotante = 1.2
+texto = "hola"
+is_true = True
+is_false = False
+#----------------------------------------------------------------------
+
+#Imprimir un texto por la terminal
+print("Hola, Python!")
